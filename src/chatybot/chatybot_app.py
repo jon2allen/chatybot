2d459ab3007c10f1c716149231469b6c6652e9b0
jon allen <jon2allen@personal_macbook.com>
2026-06-15 19:02:56 -0400

    issue with startup print version

--- src/chatybot/chatybot_app.py
+++ src/chatybot/chatybot_app.py
@@ -3324,7 +3324,7 @@ class ChatybotApp:
         print("===========================")
         print("Chatybot.py                ")
         print("Created by Jon Allen - 2025")
-        print("Version: 0.5.0             ")
+        print("Version: 0.5.2             ")
         print("===========================")
         print(
             f"Active model: {self.config_manager.get_model_config(self.config_manager.active_model_alias)['name']} (alias: {self.config_manager.active_model_alias})"
