d6e8964f1bf9e9181be8d5868dfd521e0880b23e
jon allen <jon2allen@personal_macbook.com>
2026-08-11 22:34:04 -0400

    fix(version): bump package version to 0.7.0 in __init__.py and main_loop header

--- src/chatybot/chatybot_app.py
+++ src/chatybot/chatybot_app.py
@@ -7385,7 +7385,7 @@ class ChatybotApp:
         print("===========================")
         print("Chatybot.py                ")
         print("Created by Jon Allen - 2026")
-        print("Version: 0.6.4             ")
+        print("Version: 0.7.0             ")
         lang_display = self.i18n.get_ui_string("native_lang_display", "Language: English")
         print(f"{lang_display:<27}")
         print("===========================")
